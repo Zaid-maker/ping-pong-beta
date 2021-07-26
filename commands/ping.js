f840adf1b369a87aec386bfe8ce065ac5a4a1121
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-07-26 20:29:35 +0500

    lel

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name = 'ping',
-    description: 'Tells the bots latency',
-    async execute(client, message, args, Discord) {
-        message.channel.send(`My ping: ${client.ws.ping}ms`);
-    }
-}
