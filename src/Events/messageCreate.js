38538984ce8ac4446fb10219a2a9f62c785d361e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-08-27 21:51:17 +0500

    Fixes

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -11,9 +11,9 @@ module.exports = new Event("messageCreate", (client, message) => {
 
 	if (!command) return message.reply(`${args[0]} is not a valid command!`);
 
-	const permission = message.member.permission.has(command.permission, true);
+	const permission = message.member.permissions.has(command.permission, true);
 
-	if (!permission) message.reply(`You dont have permission \`${command.permission}\` to run this command.`)
+	if (!permission) return message.reply(`You do not have the permission \`${command.permission}\` to run this command!`);
 
 	if (!["BOTH", "TEXT"].includes(command.type)) return message.reply("This command is only available via Slash Command!");
 
