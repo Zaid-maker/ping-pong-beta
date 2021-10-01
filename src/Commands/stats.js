5a4ece4983acf64e7e0f05e6b250b601c6aa3587
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-10-02 00:20:08 +0500

    New Changes

--- src/Commands/stats.js
+++ src/Commands/stats.js
@@ -2,16 +2,26 @@ const Command = require('../Structures/Command');
 
 const { MessageEmbed } = require('discord.js');
 
+const feroms = require('fero-ms');
+
 module.exports = new Command({
     name: 'stats',
     description: 'shows bots statistics.',
     type: 'BOTH',
     slashCommandOptions: [],
     async run(message, args, client) {
+        let uptime = client.uptime();
+        const shortenedUptime = `\`\`\`${feroms.ms(uptime)}\`\`\``;
         let embed = new MessageEmbed()
-        .setTitle('Statistics of the bot.')
-        .addField('Servers', `${client.guilds.cache.size}`)
-        .addField('Users', `${client.users.cache.size}`)
+        .setTitle(`${client.user.username} stats`)
+        .addFields(
+            { name: "Servers:", value: `\`\`\`${client.guilds.cache.size}\`\`\``, inline: true },
+            { name: "Users:", value: `\`\`\`${client.users.cache.size}\`\`\``, inline: true },
+            { name: "Channels:", value: `\`\`\`${client.channels.cache.size}\`\`\``, inline: true },
+            { name: "Uptime:", value: shortenedUptime, inline: true },
+            { name: "Ping:", value: `\`\`\`${Math.round(client.ws.ping)} ms\`\`\``, inline: true },
+            { name: "RAM:", value: `\`\`\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\`\`\``, inline: true }
+        )
         message.reply({ embeds: [embed] })
     }
 })
